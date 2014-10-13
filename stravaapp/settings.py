85168b5ba942eb36fc2588910a1efb7d44f67e66
Niki Macomber <nikicrisler@gmail.com>
2014-10-13 15:43:46 -0700

    initial commit to heroku

--- stravaapp/settings.py
+++ stravaapp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '%t@p9=q&*j###esig%nr%*#^i(v_%yy483(x(brigw+)d2yd70'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
@@ -63,8 +63,8 @@ WSGI_APPLICATION = 'stravaapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'stravaapp',
     }
 }
 
@@ -86,7 +86,27 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
+
 LOGIN_REDIRECT_URL = 'index'
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 
 
 try:
--- stravaapp/urls.py
+++ stravaapp/urls.py
@@ -1,11 +1,12 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
     url(r'^$', 'stravagrab.views.index', name='index'),
-    url(r'^profile', 'stravagrab.views.profile', name='profile'),
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
     url(r'^stravaAuth/', 'stravagrab.views.stravaAuth', name='stravaAuth'),
@@ -15,3 +16,6 @@ urlpatterns = patterns('',
     # url(r'^stravaAuthenticated/', 'stravagrab.views.stravaAuthenticated', name='stravaAuthenticated'),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_type=settings.MEDIA_ROOT)
